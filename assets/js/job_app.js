d5b341431e51c776bbef632d7bcf33aac8bcdea8
Jason Sanford <jasonsanford@gmail.com>
2015-07-23 20:49:25 -0400

    Live update progress bar.

--- assets/js/job_app.js
+++ assets/js/job_app.js
@@ -1,3 +1,5 @@
+var request = require('superagent');
+
 var Map = require('./map');
 
 function JobApp () {
@@ -11,6 +13,36 @@ function JobApp () {
   });
   boundsLayer.addTo(map);
   map.fitBounds(boundsLayer.getBounds());
+
+  var inerval;
+  var progressBarElem = document.querySelectorAll('.progress-bar')[0];
+
+  function checkProgress () {
+    request
+      .get('/jobs/' + window.mbtileify.jobId + '.json')
+      .end(processProgress)
+  }
+
+  function processProgress (error, resp) {
+    if (error) {
+      window.clearInterval(interval);
+      console.log('Error fetching progress: ', error);
+      return;
+    }
+
+    var body = resp.body;
+    var progressPercentage = body.progress + '%'
+
+    progressBarElem.style.width = progressPercentage;
+    progressBarElem.innerText = progressPercentage;
+
+    if (body.progress >= 100) {
+      window.clearInterval(interval);
+      console.log('Done!')
+    }
+  }
+
+  interval = window.setInterval(checkProgress, 3000)
 }
 
 module.exports = JobApp;
